04a9659d3c2262cabbce4edd05cfc747b9e3acac
paraler <archparal@gmail.com>
2016-08-17 20:25:38 +0800

    new file added

--- PageRankDemo.java
+++ PageRankDemo.java
@@ -23,8 +23,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Scanner;
+//import java.util.Scanner;
 import java.util.TreeMap;
+
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.TableColumn;
 
 //import PageRank.focusCount;
 import jxl.Sheet;
@@ -73,7 +80,11 @@ public class PageRankDemo
 	//关于输入界面的年变量控制
 	Frame frame=new Frame("pagerank");
 	FileDialog file=new FileDialog(frame,"打开文件",FileDialog.LOAD);
-	TextField textfield=new TextField(10);
+	//TextField textfield=new TextField(10);
+	String str1[] = {"1", "2", "3", "4","5","6"};  
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	JComboBox jcb=new JComboBox(str1);
+	JLabel label=new JLabel("迭代次数");
 	TextField textfield1=new TextField(15);
 	Button button1=new Button("确认");
 	Panel p=new Panel();
@@ -81,6 +92,10 @@ public class PageRankDemo
 	Panel p2=new Panel();
 	TextArea text=new TextArea();
 	Button button=new Button("选择文件");
+	
+	
+	//最后输出到表格
+	JTable table;
 	
 	//关于后台计算的变量控制
 	//设置日期格式
@@ -107,6 +122,8 @@ public class PageRankDemo
 	double[] rankMartix;
 	//计算后的rank值
 	double[][] rankResult;
+	String str;
+	
 	
 	//读取数据的方法
 	public TreeMap<String,Integer> read()
@@ -137,7 +154,7 @@ public class PageRankDemo
 			wsheet.addCell(label);  //写标题
 			  
 			
-			int i=1;
+			int i=0;
             String substr=null;
             for(int z=1;z<rows;z++)
 			{
@@ -153,16 +170,27 @@ public class PageRankDemo
             label=new Label(0,z,str);
             wsheet.addCell(label);
               
+            String list=sheet.getCell(4,z).getContents();  //第z用户的总关注ID
+            String[] b = list.split(",");
+            
             String g=sheet.getCell(3,z).getContents();
+            //筛去不符合条件的数据，当大数据时b[0].length()<9，当小数据时<1
+            if((list.contains("#"))||(b[0].length()<9))
+            	g="0";
+            
             label=new Label(1,z,g);
             wsheet.addCell(label);
-              
-            if(g.compareTo("0")>0) 
+            
+            
+       	   
+       	    
+            if(g.compareTo("0")>0)
             {
-				String list=sheet.getCell(4,z).getContents();  //第z用户的总关注ID
-				String[] b = list.split(",");
+			  
+				
 			    for (int j = 0; j < b.length; j++) 
-			    {
+			    {  
+			       
 			    	if(!tree.containsKey(b[j]))
 					{
 	   					tree.put(b[j],i);
@@ -188,6 +216,7 @@ public class PageRankDemo
 	   					substr=substr+str+",";
 					}
 				  }
+			    
 			    label=new Label(2,z,substr);
 				wsheet.addCell(label);
 				substr=null;
@@ -196,7 +225,7 @@ public class PageRankDemo
 		}//读入数据结束
 		
 		//输出总共处理的数据	
-		//System.out.println(N);
+		System.out.println(N);
 		wwb.write(); 
 		// 关闭文件   
 	    wwb.close();   
@@ -221,7 +250,9 @@ public class PageRankDemo
 		*/
 		 for(int z=1;z<rows;z++)
 		{
-			String ID=sheet.getCell(0,z).getContents();//ID为row,用户关注数为行数,用户关注的人数为初始prvalue;//0代表列数，z代表行数
+			 //ID为row,用户关注数为行数,用户关注的人数为初始prvalue;
+			 //0代表列数，z代表行数
+			String ID=sheet.getCell(0,z).getContents();
 			//System.out.print("ID is:  "+ID+"\t");
 			int B=Integer.parseInt(ID);
 			String g=sheet.getCell(1,z).getContents();
@@ -240,7 +271,7 @@ public class PageRankDemo
 					{
 						//System.out.println("list is: "+b[j]);
 						int A=Integer.parseInt(b[j]);
-						threeValueList.add(new focusCount(A-1,B-1,C));//将值送进三元组表中，并保存到List中
+						threeValueList.add(new focusCount(A,B,C));//将值送进三元组表中，并保存到List中
 					}
 					arrayList.add(threeValueList);
 				} 	
@@ -261,7 +292,7 @@ public class PageRankDemo
 		//System.out.println(k);
 		for(int i=0;i<N;i++)
 		{
-			rankMartix[i]=k;
+			rankMartix[i]=1.0;
 		}
 	}
 	
@@ -310,6 +341,7 @@ public class PageRankDemo
 	{
 		n=N;
 		//排序输出，希尔排序，复杂度为lognN
+		//;;System.out.println(rankResult.length);
 		while(true)
 		{
 			n = n / 2;
@@ -318,12 +350,15 @@ public class PageRankDemo
 				for(int i=x+n;i<rankResult.length;i=i+n)
 				{
 				double temp =rankResult[i][0];
+				double temp1 =rankResult[i][1];
 			    int j;
 			    for(j=i-n;j>=0&&rankResult[j][0]<temp;j=j-n)
 			    {
 			    	rankResult[j+n][0] = rankResult[j][0];
+			    	rankResult[j+n][1]=rankResult[j][1];
 			    }
 			    rankResult[j+n][0] = temp;
+			    rankResult[j+n][1]=temp1;
 			    }
 			 }
 			  if(n == 1)
@@ -333,6 +368,87 @@ public class PageRankDemo
 		}
 		
 	}
+	
+	//搜索昵称的函数
+	public String searchname(Object k) 
+	{    String name=null;
+		// TODO Auto-generated method stub
+    try{
+		Workbook book=Workbook.getWorkbook(new File(filepath));
+		Sheet sheet=book.getSheet(0);//0代表第一个工作表对象
+		int rows=sheet.getRows();
+		 for(int z=1;z<rows;z++)
+			{
+			 String ID=sheet.getCell(0,z).getContents();
+			  if (ID.equals((String)k))
+			  {
+			  name=sheet.getCell(1,z).getContents();
+			//  System.out.println("!!!");
+			  }
+			}
+     }catch(Exception e){
+			e.printStackTrace();
+		}
+		return name;
+	}
+	
+	
+	public void outPut(){
+		//数据以表格形式输出
+			String str;
+			Object[][] tableData=new Object[n][4];
+			for(int i=0;i<n;i++)
+			{
+				@SuppressWarnings("rawtypes")
+				Iterator it = tree.keySet().iterator(); 
+				//遍历查找对应的真实ID
+				while (it.hasNext())
+				{
+					Object k=it.next();
+					if(rankResult[i][1]==tree.get(k))
+					{
+						//输出排名
+						//text.append("第"+(i+1)+"名"+it.next()+"\n");
+						//text.append(String.valueOf(rankResult[i][0])+"\n");
+						tableData[i][0]=i+1;
+						tableData[i][1]=rankResult[i][0];
+						tableData[i][2]=k;
+						str=searchname(k);
+						if (str!=null)
+							tableData[i][3]=str;
+						else
+							tableData[i][3]="无";
+					} 
+				}
+			
+			}
+			Object[] columnTitle={"名次","PR值","用户ID","昵称"};
+			table=new JTable(tableData,columnTitle);
+			//名次列的宽度
+			TableColumn rankwidth=table.getColumn(columnTitle[0]);
+			rankwidth.setMaxWidth(50);
+			//PR值列的宽度
+			TableColumn prwidth=table.getColumn(columnTitle[1]);
+			prwidth.setMinWidth(150);
+			//用户ID的宽度
+			TableColumn idwidth=table.getColumn(columnTitle[2]);
+			idwidth.setPreferredWidth(80);
+			//y用户昵称的宽度
+			TableColumn nickwidth=table.getColumn(columnTitle[3]);
+			nickwidth.setPreferredWidth(120);
+			
+			Frame tf=new Frame("PageRank排名前20");
+			tf.add(new JScrollPane(table));
+			tf.setVisible(true);
+			tf.pack();
+			tf.setSize(400,500);
+			tf.setResizable(true);
+			tf.setLocation(900, 200);
+			//此处点击表格的关闭时，两个图形框都会关闭
+			tf.addWindowListener(new MyListen());
+	}
+	
+	
 	public void init()
 	{
 		button.addActionListener(new ActionListener()
@@ -342,78 +458,101 @@ public class PageRankDemo
 			public void actionPerformed(ActionEvent e) 
 			{	
 				// TODO Auto-generated method stub
-				
+				text.setFont(new Font("黑体",1,12));
+				text.setForeground(Color.black);
 				file.setVisible(true);
-				text.setFont(new Font("黑体",1,15));
-				text.setForeground(Color.RED);
 				filepath=file.getDirectory()+file.getFile();
 				textfield1.setText(filepath);
-				
-				
-				button1.addActionListener(new ActionListener()
-				{
+				text.append("文件读取完毕 - ");
+				text.append(date.format(new Date())+"\n");
+				text.append(filepath+"\n");
 
-					@Override
-					public void actionPerformed(ActionEvent e)
-					{
+				
+				       button1.addActionListener(new ActionListener()
+				       {
+
+					    @Override
+					    public void actionPerformed(ActionEvent e)
+				      	{
 						// TODO Auto-generated method stub
-						text.append("文件读取完毕 - ");
-						text.append(date.format(new Date())+"\n");
-						text.append(filepath+"\n");
-						String value=textfield.getText();
+						String value=jcb.getSelectedItem().toString();
 						number=Integer.valueOf(value);
 						text.append("开始读入数据 - ");
 						text.append(date.format(new Date())+"\n");
+						
 						read();
 						try {
 							readNew();
-						} catch (BiffException | IOException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
+						} 
+						catch (BiffException e1)
+						{
+						    e1.printStackTrace();
 						}
+						catch (IOException  e2) 
+						{
+						    e2.printStackTrace();
+						}
+						
+						
 						initial();
 						text.append("开始计算pagerank值 - ");
 						text.append(date.format(new Date())+"\n\n");
 						rankCal();
 						sort();
-						/*
-						for(int i=0;i<N;i++)
-						{
-							System.out.println(rankResult[i][0]+"  "+rankResult[i][1]+"  "+rankMartix[i]);
-						}
-						*/
+						
 						//只输出前20个人的pagerank值
+						
 						if(N<20)
 						{
 							n=N;
 						}else{
 							n=20;
 						}
+						
+						for(int i=0;i<n;i++){
+							System.out.println(rankResult[i][0]+"  "+rankResult[i][1]);
+						}
+						outPut();
 						text.append("计算后的结果：\n");
+						
 						for(int i=0;i<n;i++)
 						{
+							@SuppressWarnings("rawtypes")
 							Iterator it = tree.keySet().iterator(); 
-							//遍历查找对应的真实ID并输出
-							while (it.hasNext())                
-							if(rankResult[i][1]==tree.get(it.next()))
+							//遍历查找对应的真实ID
+							while (it.hasNext())    
 							{
-								text.append("第"+(i+1)+"名"+it.next()+"\n");
-								text.append(String.valueOf(rankResult[i][0])+"\n");
-								// text.append(String.valueOf(rankResult[i][1])+"\n");
-							} 
-					
+								Object k=it.next();
+								if(rankResult[i][1]==tree.get(k))
+								{
+									//输出排名
+									String str=searchname(k);
+								   if (str!=null)
+									text.append("第"+(i+1)+"名"+str+"\n");
+								   else
+									   text.append("第"+(i+1)+"名"+k+"\n");
+								   
+									text.append(String.valueOf(rankResult[i][0])+"\n");
+									// text.append(String.valueOf(rankResult[i][1])+"\n");
+								} 
+							}
 						}
+
 						text.append(date.format(new Date())+"\n");
-						
+						JOptionPane.showMessageDialog(null, "计算完毕！");
 					}
 			
 				});
-				//button1 Listener END
+				//Button1 Listener END
 			}
 			
+
 		});
-		//button Listener END
-		textfield.setText("输入迭代次数");
+		//Button END
+		
+		
+		
+		//	textfield.setText("输入迭代次数");
 		frame.setSize(400,500);
 		button.setSize(200, 100);
 		//textfield.setSize(300,100);
@@ -421,15 +560,19 @@ public class PageRankDemo
 		p1.add(textfield1);
 		p1.add(button);
 		
-		p2.add(textfield);
+		p2.add(label);
+		p2.add(jcb);
 		p2.add(button1);
 		
 		p.add(p1);
 		p.add(p2);
+		
+		//设置窗口属性
 		text.setSize(300,400);
 		frame.addWindowListener(new MyListen());
 		frame.add(p,BorderLayout.NORTH);
 		frame.add(text,BorderLayout.CENTER);
+		//frame.add(table,BorderLayout.SOUTH);
 		frame.setLocation(500, 200);
 		frame.setVisible(true);
 	}
